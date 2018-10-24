d56def703a9f2e81fe4d82a15e372d11e2bb6d51
Wojciech Baczyński <wp.baczynski@gmail.com>
2018-10-24 09:15:32 +0200

    Added delete todo funcionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,11 @@
 $("li").on("click", function(){
     $(this).toggleClass("completed");    
 });
+
+
+$("li span").on("click", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
